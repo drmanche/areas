21ae3f9e644a1188acc128e2d6c63ce449707c24
DrManche <82394874+DrManche@users.noreply.github.com>
2022-12-07 03:12:17 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+function areaTrian() {document.getElementById("areaTri").innerHTML = ((parseFloat(document.getElementById("base").value)) * (parseFloat(document.getElementById("altura").value))/ 2);};
+function areaCircu() {document.getElementById("areaCir").innerHTML = (((parseFloat(document.getElementById("radio").value)) ** 2) * 3.1416);};
+function areaCuad() {document.getElementById("areaCua").innerHTML = ((parseFloat(document.getElementById("lado").value)) ** 2);};
+function areaRecta() {document.getElementById("areaRec").innerHTML = ((parseFloat(document.getElementById("baseR").value)) * (parseFloat(document.getElementById("alturaR").value)));};
